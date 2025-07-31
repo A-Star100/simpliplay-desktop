238684b1670da517223615c33da22ae730fc94ce
Anirudh Sevugan <sonicforces207@gmail.com>
2025-07-30 19:17:17 -0500

    fix error with update system

--- simpliplay/main.js
+++ simpliplay/main.js
@@ -6,7 +6,7 @@ const { pathToFileURL } = require("url");
 const { checkForUpdate } = require('./updateChecker');
 let gpuAccel = "";
 let didRegisterShortcuts = false;
-let version = "2.0.3.1"
+let version = "2.0.4.0"
 
 if (process.platform === 'darwin') {
   if (process.argv.includes('--use-gl')) {
