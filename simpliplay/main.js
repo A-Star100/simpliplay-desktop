448030e57c7fc07851724026f55e8385210a1e89
Anirudh Sevugan <sonicforces207@gmail.com>
2026-01-18 13:08:51 -0600

    Update version number to 2.2.0.0

--- simpliplay/main.js
+++ simpliplay/main.js
@@ -6,7 +6,7 @@ const { pathToFileURL } = require("url");
 const { checkForUpdate } = require('./updateChecker');
 let gpuAccel = "";
 let didRegisterShortcuts = false;
-let version = "2.1.9.0" // regular electron update once in a while
+let version = "2.2.0.0" // regular electron update once in a while
 
 if (process.platform === 'darwin') {
   if (process.argv.includes('--use-gl')) {
