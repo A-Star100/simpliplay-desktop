7e1ab1032e96c0097114b3e0cec684b725032b20
Anirudh Sevugan <sonicforces207@gmail.com>
2026-03-09 17:45:49 -0500

    Create basic-test.spec.js

---
+++ simpliplay/tests/basic-test.spec.js
@@ -0,0 +1,27 @@
+const { _electron: electron } = require('playwright');
+
+(async () => {
+  // launch
+  const electronApp = await electron.launch({ args: ['main.js'] });
+
+  // do evaluation expression in the context of electron
+  const appPath = await electronApp.evaluate(async ({ app }) => {
+    // runs in main Electron
+    return app.getAppPath();
+  });
+  console.log(appPath);
+
+  // get first window
+  const window = await electronApp.firstWindow();
+  // print its title
+  console.log(await window.title());
+  // do a screenshot
+  await window.screenshot({ path: 'intro.png' });
+  // direct electron console to node terminal.
+  window.on('console', console.log);
+  // click a button
+  // TODO: change this later
+  await window.click('text=Go back');
+  // exit
+  await electronApp.close();
+})();
