b02342a406ed563413214db00c4f6120f5528903
Anirudh Sevugan <sonicforces207@gmail.com>
2026-03-09 19:54:37 -0500

    change window.fill syntax

--- simpliplay/tests/basic-test.spec.js
+++ simpliplay/tests/basic-test.spec.js
@@ -30,7 +30,7 @@ const { _electron: electron } = require('playwright');
   // direct electron console to node terminal.
   window.on('console', console.log);
   await window.click('text=Enter a URL')
-  await window.fill('#urlInput', 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8')
+  await window.fill('input[name="urlInput"]', 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8');
   await window.click('text=Submit')
   await delay(3000);
   
