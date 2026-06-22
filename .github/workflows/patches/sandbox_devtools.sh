6e0b7403fde9b83dad8c13a1a661a2cde9de1b49
Anirudh Sevugan <sonicforces207@gmail.com>
2026-06-22 18:22:47 -0500

    add janky sed to remove separator and Inspect btn instead

--- .github/workflows/patches/sandbox_devtools.sh
+++ .github/workflows/patches/sandbox_devtools.sh
@@ -2,4 +2,4 @@ js="main.js"
 # set devtools to false in webPreferences
 sed -i '102s/$/, \n    devTools: false,/' $js
 # delete separator and inspect btn
-sed -i '147,148d'
+sed -i "/contextMenu.append(new MenuItem({ type: 'separator' }));/,/contextMenu.append(new MenuItem({ label: 'Inspect', click: () => mainWindow.webContents.openDevTools() }));/d" $js
