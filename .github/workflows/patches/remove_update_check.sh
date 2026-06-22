0c4ae29feff1b5b07de3629432a091c73615205f
Anirudh Sevugan <sonicforces207@gmail.com>
2026-06-21 20:16:13 -0500

    Create remove_update_check.sh

---
+++ .github/workflows/patches/remove_update_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+js="main.js"
+
+# last ditch effort to remove update check in main.js
+# it won't change too often anyway
+sed -i '169,186d' "$js"
+
+echo "Tried to remove update check (lines 169-186 as of latest edit)."
+echo "Also note that if the line numbers are wrong, this patch needs to be edited"
+cat "$js"
