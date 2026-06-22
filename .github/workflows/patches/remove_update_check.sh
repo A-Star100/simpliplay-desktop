05726dc0bace7cadadc5cc19681fcbcad65d1ea0
Anirudh Sevugan <sonicforces207@gmail.com>
2026-06-21 20:25:27 -0500

    delete lines 201-216 as well

--- .github/workflows/patches/remove_update_check.sh
+++ .github/workflows/patches/remove_update_check.sh
@@ -3,7 +3,7 @@ js="simpliplay/main.js"
 
 # last ditch effort to remove update check in main.js
 # it won't change too often anyway
-sed -i '169,186d' "$js"
+sed -i '169,186d; 201,216d' "$js"
 
 echo "Tried to remove update check (lines 169-186 as of latest edit)."
 echo "Also note that if the line numbers are wrong, this patch needs to be edited"
