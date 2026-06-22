266eed38781f9a810891f419cb5b5c103010b00c
Anirudh Sevugan <sonicforces207@gmail.com>
2026-06-22 18:27:01 -0500

    use another janky sed to fixup devtools edit

--- .github/workflows/patches/sandbox_devtools.sh
+++ .github/workflows/patches/sandbox_devtools.sh
@@ -1,5 +1,5 @@
 js="simpliplay/main.js"
 # set devtools to false in webPreferences
-sed -i '102s/$/, \n    devTools: false,/' $js
+sed -i '102a\    devTools: false,' "$js"
 # delete separator and inspect btn
 sed -i "/contextMenu.append(new MenuItem({ type: 'separator' }));/,/contextMenu.append(new MenuItem({ label: 'Inspect', click: () => mainWindow.webContents.openDevTools() }));/d" $js
